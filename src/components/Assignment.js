e8c612ae5c0f379a159444770d5ad6cd78b8de7a
CAbdoulaye <c_faye@mail.fhsu.edu>
2024-05-07 15:34:16 -0500

    added ustate and remove and completed options

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+import './styles.css'; // Import your CSS file
 
-export default function Assignment({id, title, description}) {
-  return (
-    <div>
-      <h3>{id} {title}</h3>
-      <p>{description}</p>
-    </div>
-  )
+
+export default function Assignment({title, description, completionStatus}) {
+  const [removed, setRemoved] = useState("False");
+  if (removed !== "True"){
+    return (
+      <div className="assignment">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <label for="scales">Completed</label>
+        <input type="checkbox" id="scales" name="scales"/>
+        <br/>
+        <label for="scales">Remove: </label>
+        <input type="submit" value="X" onClick={() =>{setRemoved("True")}}/>
+      </div>
+    )
+  }
+  else{
+    return (<div></div>)
+  }
+
 }
--- src/components/AssignmentsData.js
+++ src/components/AssignmentsData.js
@@ -2,27 +2,32 @@ const assignmentData = [
   {
     id: 1,
     title: "History",
-    description: "Read Chapter 17 and complete assignments 4-12"
+    description: "Read Chapter 17 and complete assignments 4-12",
+    completionStatus: "True"
   },
   {
     id: 2,
     title: "English",
-    description: "Write Essay about leadership and influence"
+    description: "Write Essay about leadership and influence",
+    completionStatus: "True"
   },
   {
     id: 3,
     title: "Math",
-    description: "Solve Pythagorean Theorem problem"
+    description: "Solve Pythagorean Theorem problem",
+    completionStatus: "False"
   },
   {
     id: 4,
     title: "Biology",
-    description: "Complete Heat VS Coler Project"
+    description: "Complete Heat VS Coler Project",
+    completionStatus: "True"
   },
   {
     id: 5,
     title: "Technology",
-    description: "Setup Lunix operating system on computer"
+    description: "Setup Lunix operating system on computer",
+    completionStatus: "False"
   }
 ];
 
