bf9ff479cb043d4f0befe8d5768d6959a05fa2dd
CAbdoulaye <c_faye@mail.fhsu.edu>
2024-05-07 20:11:10 -0500

    included subtasks to task array objects

--- src/components/AssignmentsData.js
+++ src/components/AssignmentsData.js
@@ -3,31 +3,41 @@ const assignmentData = [
     id: 1,
     title: "History",
     description: "Read Chapter 17 and complete assignments 4-12",
-    completionStatus: "True"
+    completionStatus: "True",
+    removed: false,
+    subTasks: {}
   },
   {
     id: 2,
     title: "English",
     description: "Write Essay about leadership and influence",
-    completionStatus: "True"
+    completionStatus: "True",
+    removed: false,
+    subTasks: {}
   },
   {
     id: 3,
     title: "Math",
     description: "Solve Pythagorean Theorem problem",
-    completionStatus: "False"
+    completionStatus: "False",
+    removed: false,
+    subTasks: {}
   },
   {
     id: 4,
     title: "Biology",
     description: "Complete Heat VS Coler Project",
-    completionStatus: "True"
+    completionStatus: "True",
+    removed: false,
+    subTasks: {}
   },
   {
     id: 5,
     title: "Technology",
     description: "Setup Lunix operating system on computer",
-    completionStatus: "False"
+    completionStatus: "False",
+    removed: false,
+    subTasks: {}
   }
 ];
 
